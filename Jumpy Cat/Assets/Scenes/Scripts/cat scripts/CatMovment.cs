6426a6f774fd95e2027bc7c29831e60a28ec5036
duhaemad <75424796+duhaemad@users.noreply.github.com>
2021-02-16 23:17:00 +0200

    moving the cat

--- Jumpy Cat/Assets/Scenes/Scripts/cat scripts/CatMovment.cs
+++ Jumpy Cat/Assets/Scenes/Scripts/cat scripts/CatMovment.cs
@@ -7,7 +7,7 @@ public class CatMovment : MonoBehaviour
 {
     private BlockManager blockManager;
     [SerializeField]
-    private float speed=0.3f, height=0.5f;
+    private float speed=0.3f, height=2f;
     [SerializeField]
     private AudioClip catDie;
     private AudioSource audioManager;
@@ -51,6 +51,54 @@ public class CatMovment : MonoBehaviour
     }
     void MoveCat()
     {
+        KeyCode key =(KeyCode) keyArray[0];
+
+        if (key == KeyCode.UpArrow) {
+            audioManager.Play(); //jumpsound
+            anim.Play("ready");
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            Vector3 pos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1f); //going forward in 3d is the z axis
+            transform.DOJump(pos,height,1,speed);
+            //blockManager.LeaveLandedBlock();
+
+            anim.SetTrigger("jump");
+        }
+
+        if (key == KeyCode.DownArrow)
+        {
+            audioManager.Play(); //jumpsound
+            anim.Play("ready");
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            Vector3 pos = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1f); //going forward in 3d is the z axis
+            transform.DOJump(pos, height, 1, speed);
+            //blockManager.LeaveLandedBlock();
+
+            anim.SetTrigger("jump");
+        }
+
+        if (key == KeyCode.RightArrow)
+        {
+            audioManager.Play(); //jumpsound
+            anim.Play("ready");
+            transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+            Vector3 pos = new Vector3(transform.position.x + 1f, transform.position.y, transform.position.z ); //going forward in 3d is the z axis
+            transform.DOJump(pos, height, 1, speed);
+            //blockManager.LeaveLandedBlock();
+
+            anim.SetTrigger("jump");
+        }
+
+        if (key == KeyCode.LeftArrow)
+        {
+            audioManager.Play(); //jumpsound
+            anim.Play("ready");
+            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+            Vector3 pos = new Vector3(transform.position.x - 1f, transform.position.y, transform.position.z); //going forward in 3d is the z axis
+            transform.DOJump(pos, height, 1, speed);
+            //blockManager.LeaveLandedBlock();
+
+            anim.SetTrigger("jump");
+        }
 
     }
     // Update is called once per frame
